0f326fe7f56c88e844b10ff2b24e2670819f5a8c
Jason Scheirer <jason.scheirer@gmail.com>
2008-11-03 13:29:35 -0800

    Build batch script

---
+++ build.bat
@@ -0,0 +1,4 @@
+c:\python25\python.exe setup.py bdist_wininst
+c:\python25\python.exe setup.py sdist --formats=gztar,zip
+copy dist\*.* \\ironpaw\pub\jasons\arcrest
+copy documentation\html\*.* \\ironpaw\pub\jasons\arcrest\documentation\
