1258d8723ec4ed6d5e159da6a96ad60966911ee3
Jason Scheirer <jason.scheirer@gmail.com>
2008-11-18 15:38:49 -0800

    Merge tsts with ironpaw instead of copying

--- build.bat
+++ build.bat
@@ -2,4 +2,5 @@ c:\python25\python.exe setup.py bdist_wininst
 c:\python25\python.exe setup.py sdist --formats=gztar,zip
 copy dist\*.* \\ironpaw\pub\jasons\arcrest
 copy documentation\html\*.* \\ironpaw\pub\jasons\arcrest\documentation\
+"C:\Program Files\WinMerge\WinMergeU.exe" tests.py \\ironpaw\pub\jasons\arcrest\tests.py
 hg history > \\ironpaw\pub\jasons\arcrest\changelog.txt
