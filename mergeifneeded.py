b60b063e3b4ece56c88bb3c547a06706510ef42b
Jason Scheirer <jason.scheirer@gmail.com>
2008-12-05 17:46:55 -0800

    Fixed test merge script

--- mergeifneeded.py
+++ mergeifneeded.py
@@ -1,11 +1,12 @@
 import os
 import sha
 
-file1 = r"tests.py"
-file2 = r"\\ironpaw\pub\jasons\arcrest\tests.py"
-cs1, cs2 = sha.new(open(file1).read()).hexdigest(), \
-           sha.new(open(file2).read()).hexdigest()
+def mergeifneeded(file1, file2):
+    cs1, cs2 = sha.new(open(file1).read()).hexdigest(), \
+               sha.new(open(file2).read()).hexdigest()
+    if cs1 != cs2:
+        os.popen(r'"C:\Program Files\WinMerge\WinMergeU.exe" %s %s' % 
+                   (file1, file2))
 
-if cs1 != cs2:
-    os.popen(r'"C:\Program Files\WinMerge\WinMergeU.exe" %s %s' % 
-               (file1, file2))
+mergeifneeded(r"tests.py", r"\\ironpaw\pub\jasons\arcrest\tests.py")
+mergeifneeded(r"guitest.py", r"\\ironpaw\pub\jasons\arcrest\guitest.py")
