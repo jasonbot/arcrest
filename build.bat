3c3b753db6147dc5fe5f6e35c48a87b7b1c18593
Jason Scheirer <jason.scheirer@gmail.com>
2009-06-18 15:03:19 -0700

    Deploy to \\jscheirer\pub\

--- build.bat
+++ build.bat
@@ -2,9 +2,9 @@ c:\python26\python.exe setup.py clean
 c:\python26\python.exe setup.py install
 c:\python26\python.exe setup.py bdist_wininst
 c:\python26\python.exe setup.py sdist --formats=gztar,zip
-copy dist\*.* \\ironpaw\pub\jasons\arcrest
-copy documentation\html\*.* \\ironpaw\pub\jasons\arcrest\documentation\
+copy dist\*.* c:\pub\arcrest
+copy documentation\html\*.* c:\pub\arcrest\documentation\
 python mergeifneeded.py
-hg history > \\ironpaw\pub\jasons\arcrest\changelog.txt
-unix2dos \\ironpaw\pub\jasons\arcrest\changelog.txt
-del \\ironpaw\pub\jasons\arcrest\*.bak
+hg history > c:\pub\arcrest\changelog.txt
+unix2dos c:\pub\arcrest\changelog.txt
+del c:\pub\arcrest\*.bak
