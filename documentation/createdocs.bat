5ade9aa5a5cbf479a76ba0473c7e1eca895e9ad3
Jason Scheirer <jason.scheirer@gmail.com>
2010-10-19 16:53:50 -0700

    Updated docs

--- documentation/createdocs.bat
+++ documentation/createdocs.bat
@@ -1,1 +1,1 @@
-c:\Python27\Scripts\epydoc.py --dotpath="c:\Program Files\Graphviz2.24\bin\dot.exe" --graph-font="Calibri" -v --graph=all --show-imports --no-private arcrest
+c:\python27\python.exe c:\Python27\Scripts\epydoc --dotpath="c:\Program Files\Graphviz2.24\bin\dot.exe" --graph-font="Calibri" -v --graph=all --show-imports --no-private arcrest
