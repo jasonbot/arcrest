69f24e26bca728c3dcc74c2f06b4218e73f0f0fe
Jason Scheirer <jason.scheirer@gmail.com>
2012-03-22 14:11:41 -0700

    Work-in-progress for ArcGIS online APIs

---
+++ arcrest/ago.py
@@ -0,0 +1,43 @@
+"""Represents the ArcGIS online REST APIs"""
+
+from arcrest import server
+
+__all__ = ['AGORoot', 'Community', 'Content', 'Portals']
+
+class AGORoot(server.RestURL):
+    def __init__(self, url, username=None, password=None,
+                 token=None, generate_token=False,
+                 expiration=60):
+        url_list = list(urlparse.urlsplit(url))
+        if not url_list[2].endswith('/'):
+            url_list[2] += "/"
+        url = urlparse.urlunsplit(url_list)
+        if username is not None and password is not None:
+            self._pwdmgr.add_password(None,
+                                      url,
+                                      username,
+                                      password)
+        if token:
+            self.__token__ = token
+        elif generate_token:
+            self.__generateToken(url, username, password, expiration)
+        super(AGORoot, self).__init__(url)
+    def search(self, q=None, bbox=None, start=None, num=None,
+               sortField=None, sortOrder=None):
+        return self._get_subfolder("./search", 
+                                          server.JsonPostResult,
+                                          {'q': q,
+                                           'bbox': bbox,
+                                           'start': start,
+                                           'num': num,
+                                           'sortField': sortField,
+                                           'sortOrder': sortOrder})
+    @property
+    def community(self):
+        return self._get_subfolder("./community/", Community)
+    @property
+    def content(self):
+        return self._get_subfolder("./content/", Content)
+    @property
+    def portals(self):
+        return self._get_subfolder("./portals/", Portals)
