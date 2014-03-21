421527640ca9072c9e3965aece1e326d098ab109
"Jason Scheirer" <jason.scheirer@gmail.com>
2014-03-21 12:37:00 -0700

    Syntax fixes

--- arcrest/admin/cmdline.py
+++ arcrest/admin/cmdline.py
@@ -13,11 +13,15 @@ __all__ = ['createservice', 'manageservice', 'managesite', 'deletecache',
            'managecachetiles', 'createcacheschema',
            'convertcachestorageformat', 'importcache', 'exportcache']
 
-IN_WINE = any(e in os.environ for e in ('WINELOADERNOEXEC', 'DOTWINE', 'WINEPREFIX', 'WINEDEBUG'))
-PROG_NAME = (sys.argv[0] if not IN_WINE else os.path.splitext(sys.argv[0])[0])
+IN_WINE = any(e in os.environ for e in ('WINELOADERNOEXEC',
+                                        'DOTWINE',
+                                        'WINEPREFIX',
+                                        'WINEDEBUG'))
+PROG_NAME = os.path.basename((sys.argv[0] 
+                              if not IN_WINE
+                              else os.path.splitext(sys.argv[0])[0]))
 
-shared_args = argparse.ArgumentParser(prog=PROG_NAME,
-                                      add_help=False)
+shared_args = argparse.ArgumentParser(prog=PROG_NAME,add_help=False)
 shared_args.add_argument('-u', '--username', 
                          required=True,
                          help='Description: Username for Server')
@@ -95,7 +99,7 @@ def wait_on_tool_run(result_object, silent=False):
         for message_object in messages[message_length:]:
             print (message_object.description)
 
-createserviceargs = argparse.ArgumentParser(description='Creates a service',
+createserviceargs = argparse.ArgumentParser(prog=PROG_NAME,description='Creates a service',
                                             parents=[shared_args])
 createserviceargs.add_argument('-C', '--cluster',
                                nargs='?',
@@ -172,7 +176,7 @@ def createservice(action):
                                          '{}/delete'.format(id))
                 compat.urllib2.urlopen(delete_url, '').read()
 
-manageserviceargs = argparse.ArgumentParser(description=
+manageserviceargs = argparse.ArgumentParser(prog=PROG_NAME,description=
                                                 'Manages/modifies a service',
                                             parents=[shared_args])
 manageserviceargs.add_argument('-n', '--name',
@@ -249,7 +253,7 @@ def manageservice(action):
             with action("deleting service"):
                 return service.delete()
 
-managesiteargs = argparse.ArgumentParser(description=
+managesiteargs = argparse.ArgumentParser(prog=PROG_NAME,description=
                                                 'Manages/modifies a site',
                                             parents=[shared_args])
 managesiteargs.add_argument('-A', '--add-machines',
@@ -357,7 +361,7 @@ def managesite(action):
                     print("-", cluster)
                 print()
 
-deletecacheargs = argparse.ArgumentParser(description=
+deletecacheargs = argparse.ArgumentParser(prog=PROG_NAME,description=
                                                 'Deletes a map tile cache',
                                             parents=[shared_args])
 deletecacheargs.add_argument('-n', '--name',
@@ -384,7 +388,7 @@ def deletecache(action):
         result_object = delete_cache_tool(args.name, args.instances)
         wait_on_tool_run(result_object, silent=True)
 
-managecachetilesargs = argparse.ArgumentParser(description=
+managecachetilesargs = argparse.ArgumentParser(prog=PROG_NAME,description=
                                                 'Manage a map tile cache',
                                             parents=[shared_args])
 managecachetilesargs.add_argument('-n', '--name',
@@ -438,7 +442,7 @@ def managecachetiles(action):
     else:
         print (result_object.url)
 
-createcacheschemaargs = argparse.ArgumentParser(description=
+createcacheschemaargs = argparse.ArgumentParser(prog=PROG_NAME,description=
                                              'Creates a map tile cache schema',
                                             parents=[shared_args])
 createcacheschemaargs.add_argument('-n', '--name',
@@ -507,7 +511,7 @@ def createcacheschema(action):
                                           args.tile_size.split("x")[1])
         wait_on_tool_run(result_object)
 
-convertcachestorageformatargs = argparse.ArgumentParser(
+convertcachestorageformatargs = argparse.ArgumentParser(prog=PROG_NAME,
                                             description=
                                              'Convert the format of a stored '
                                              'map cache',
@@ -540,7 +544,7 @@ def convertcachestorageformat(action):
             time.sleep(0.125)
         print ("\n".join(msg.description for msg in result_object.messages))
 
-importcacheargs = argparse.ArgumentParser(description=
+importcacheargs = argparse.ArgumentParser(prog=PROG_NAME,description=
                                              'Import a stored '
                                              'map cache',
                                             parents=[shared_args])
